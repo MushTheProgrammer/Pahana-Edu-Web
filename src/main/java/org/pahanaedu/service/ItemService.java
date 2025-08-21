2bf7600678a90633a37680d83241ec32da72ed3e
MushTheProgrammer <iammush22@gmail.com>
2025-08-21 23:50:01 +0530

    Completed item related functions part1

--- src/main/java/org/pahanaedu/service/ItemService.java
+++ src/main/java/org/pahanaedu/service/ItemService.java
@@ -1,4 +1,19 @@
 package org.pahanaedu.service;
 
+import org.pahanaedu.model.Item;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ItemService {
+    boolean addItem(Item item);
+    boolean updateItem(Item item);
+    boolean deleteItem(String itemCode);
+    Item getItemByCode(String itemCode);
+    List<Item> getAllItems();
+
+    List<Item> searchItems(String code, String name, BigDecimal minPrice, BigDecimal maxPrice, Integer minQty, Integer maxQty, int page, int size, String sort, String dir);
+    int countItems(String code, String name, BigDecimal minPrice, BigDecimal maxPrice, Integer minQty, Integer maxQty);
+
+    String getNextItemCode();
 }
